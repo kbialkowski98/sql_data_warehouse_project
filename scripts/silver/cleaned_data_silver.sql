9f2efdd7de0030a4007cc1d2f80c648bb6bf4294
kbialkowski98 <kacper.bialkowski98@gmail.com>
2025-09-22 17:01:58 +0200

    Update cleaned_data_silver.sql

--- scripts/silver/cleaned_data_silver.sql
+++ scripts/silver/cleaned_data_silver.sql
@@ -137,5 +137,20 @@ SELECT
 FROM
     bronze.erp_cust_az12
 
+--erp_loc_a101
+INSERT INTO silver.erp_loc_a101(
+    cid
+    ,cntry
+)
 
+SELECT
+    REPLACE(cid,'-','') cid
+    ,CASE
+     WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+     WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+     WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'Unknown'
+     ELSE TRIM(cntry)
+     END cntry
+FROM
+    bronze.erp_loc_a101
 
